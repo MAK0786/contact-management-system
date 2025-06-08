854a6d434c40dde2129a6c5c4a4763d50d36bf88
Mohammed Arsalan Khan <44841640+MAK0786@users.noreply.github.com>
2025-06-08 22:27:31 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
-const backendUrl = process.env.REACT_APP_BACKEND_URL || "https://contact-management-system-kqkn.onrender.com/api";
+const backendUrl = process.env.REACT_APP_BACKEND_URL || "https://contact-management-system-kqkn.onrender.com";
 export default backendUrl;
