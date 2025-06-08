4c77f59a9b1a66ea7cf7dbacd699582fa82db6f9
Mohammed Arsalan Khan <44841640+MAK0786@users.noreply.github.com>
2025-06-08 22:31:01 +0530

    Update ContactList.jsx

--- frontend/src/components/ContactList.jsx
+++ frontend/src/components/ContactList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import AddContact from "./AddContact";
-import { backendUrl } from "../config";  // Import backend URL
+import backendUrl from "../config";  // Import backend URL
 import "./ContactList.css";
 
 // Use backendUrl + /api as base URL
